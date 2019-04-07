a088aedb5e3c335aac154b6fb27b82ffd07bfc86
前川清成 <kiyonari6318@maekawakiyonarinoMacBook-puro.local>
2019-04-08 04:13:10 +0900

    画像をアップロードする機能を追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -69,6 +69,6 @@ class PicturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.require(:picture).permit(:word, :image)
+      params.require(:picture).permit(:word, :image,:image_cache)
     end
 end
