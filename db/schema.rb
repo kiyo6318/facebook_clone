0bba687a0e8cf70ddc7a97d0b18f929e89d39d32
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-08 11:24:33 +0900

    userモデルのバリデーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_215534) do
+ActiveRecord::Schema.define(version: 2019_04_08_021129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_04_07_215534) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
