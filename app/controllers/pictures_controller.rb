df90a1110ccac82da1d9f80f247cc604d8ae5eaf
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-10 18:03:21 +0900

    インデント修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -73,12 +73,12 @@ class PicturesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def picture_params
-      params.require(:picture).permit(:word, :image,:image_cache)
-    end
+  def picture_params
+    params.require(:picture).permit(:word, :image,:image_cache)
+  end
 end
