ea6d6c5606f8d9bd6435c2af18d7df99a5fb66ce
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-10 14:48:20 +0900

    ログインの有無でアクション制限 && デザイン細部修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def authenticate_user
+    if current_user == nil
+      flash[:notice] = "ログインが必要です"
+      redirect_to("/sessions/new")
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email:params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user.id)
+    @user = User.find_by(email:params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
     else
       flash[:danger] = 'ログインに失敗しました'
       render 'new'
